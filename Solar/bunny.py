8faf107bc6aeca50ddf81760311ed0fb70c8010c
stocksalesdata <theaimcreator@gmail.com>
2024-03-17 16:40:34 +0530

    sdeasd

--- Solar/bunny.py
+++ Solar/bunny.py
@@ -3,8 +3,9 @@ from BunnyCDN.Storage import Storage
 from config import Config
 
 storage_api_key=Config.STORAGE_API_KEY
-storage_api_key="7ac93fd8-09f9-4a9f-acc6c3515889-6e83-44d0"
+storage_api_key=Config.STORAGE_API_KEY
 storage_zone_name=Config.STORAGE_ZONE_NAME
+storage_zone_name="stocksales"
 
 obj_storage = Storage(storage_api_key,storage_zone_name)
 obj_cdn = CDN(Config.STORAGE_CDN)
