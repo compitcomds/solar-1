ae3a2790fb019cd3e821d7023409a4854fcf3c87
stocksalesdata <theaimcreator@gmail.com>
2024-03-17 16:39:15 +0530

    asd

--- Solar/bunny.py
+++ Solar/bunny.py
@@ -3,6 +3,7 @@ from BunnyCDN.Storage import Storage
 from config import Config
 
 storage_api_key=Config.STORAGE_API_KEY
+storage_api_key="7ac93fd8-09f9-4a9f-acc6c3515889-6e83-44d0"
 storage_zone_name=Config.STORAGE_ZONE_NAME
 
 obj_storage = Storage(storage_api_key,storage_zone_name)
